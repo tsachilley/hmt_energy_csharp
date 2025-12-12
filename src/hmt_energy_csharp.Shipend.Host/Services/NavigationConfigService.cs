02983f1cefd1e88819cc6ad8902cd344d03548bf
tsachilley <tsachille@126.com>
2025-12-12 14:57:07 +0800

    修复部分功能

--- src/hmt_energy_csharp.Shipend.Host/Services/NavigationConfigService.cs
+++ src/hmt_energy_csharp.Shipend.Host/Services/NavigationConfigService.cs
@@ -1,13 +1,14 @@
-﻿using Castle.Core.Logging;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using hmt_energy_csharp.Energy.Configs;
 using hmt_energy_csharp.Protos;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
-using RestSharp;
+using Serilog;
 using System;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace hmt_energy_csharp.Services
@@ -16,11 +17,13 @@ namespace hmt_energy_csharp.Services
     {
         private readonly ILogger<NavigationConfigService> _logger;
         private readonly IConfigService _configService;
+        private readonly IConfiguration _configuration;
 
-        public NavigationConfigService(ILogger<NavigationConfigService> logger, IConfigService configService)
+        public NavigationConfigService(ILogger<NavigationConfigService> logger, IConfigService configService, IConfiguration configuration)
         {
             _logger = logger;
             _configService = configService;
+            _configuration = configuration;
         }
 
         public override async Task<InsertResponse> Insert(InsertRequest request, ServerCallContext context)
@@ -116,6 +119,83 @@ namespace hmt_energy_csharp.Services
             {
                 _logger.LogError(ex, MethodBase.GetCurrentMethod().DeclaringType.Namespace + "_" + MethodBase.GetCurrentMethod().Name);
                 throw new Exception("210001");
+            }
+        }
+
+        public override async Task<SelectManualParamsResponse> SelectManualParams(SelectManualParamsRequest request, ServerCallContext context)
+        {
+            var response = new SelectManualParamsResponse();
+            try
+            {
+                var result = await _configService.GetList(new { IsDevice = 2, IsEnabled = 1, Number = _configuration["ShipInfo:SN"] ?? "SAD1" }.ToJson());
+                StringBuilder returnResult = new();
+                foreach (var entity in result)
+                {
+                    if (entity.Code == "ManualDraft")
+                        returnResult.Append("\"" + entity.Code + "\":" + entity.HighLimit.ToString() + ",");
+                    else if (entity.Code == "ManualBLRFC")
+                        returnResult.Append("\"" + entity.Code + "\":" + entity.HighLimit.ToString() + ",");
+                    else if (entity.Code == "ManualRunoff")
+                        returnResult.Append("\"" + entity.Code + "\":" + entity.HighLimit.ToString() + ",");
+                    else if (entity.Code == "ManualTidal")
+                        returnResult.Append("\"" + entity.Code + "\":" + entity.HighLimit.ToString() + ",");
+                }
+                returnResult.Remove(returnResult.Length - 1, 1).Insert(0, '{').Append('}');
+                response.Result = Value.Parser.ParseJson(returnResult.ToString());
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, MethodBase.GetCurrentMethod().DeclaringType.Namespace + "_" + MethodBase.GetCurrentMethod().Name);
+                response.ErrMessage = "210001";
+                return response;
+            }
+        }
+
+        public override async Task<UpdateManualParamsResponse> UpdateManualParams(UpdateManualParamsRequest request, ServerCallContext context)
+        {
+            var response = new UpdateManualParamsResponse();
+            try
+            {
+                var ManualParams = request.Parameters.ToJObject();
+                var lst = await _configService.GetList(new { IsDevice = 2, IsEnabled = 1, Number = _configuration["ShipInfo:SN"] ?? "SAD1" }.ToJson());
+                try
+                {
+                    foreach (var ManualParam in ManualParams)
+                    {
+                        try
+                        {
+                            if (lst.Any(t => t.Code == ManualParam.Key))
+                            {
+                                var dto = lst.FirstOrDefault(t => t.Code == ManualParam.Key);
+                                dto.HighLimit = Convert.ToDecimal(ManualParam.Value);
+                                dto.update_time = DateTime.UtcNow;
+                                await _configService.Update(Convert.ToInt32(dto.Id), dto);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex, "{Namespace}_{MethodName}", MethodBase.GetCurrentMethod()?.DeclaringType?.Namespace + "_" + ManualParam.Key, MethodBase.GetCurrentMethod()?.Name);
+                        }
+                    }
+
+                    StaticEntities.StaticEntities.Configs.Clear();
+
+                    response.Result = 1;
+                }
+                catch (Exception ex)
+                {
+                    response.Result = 0;
+                    _logger.LogError(ex, MethodBase.GetCurrentMethod().DeclaringType.Namespace + "_" + MethodBase.GetCurrentMethod().Name);
+                    response.ErrMessage = "210001";
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, MethodBase.GetCurrentMethod().DeclaringType.Namespace + "_" + MethodBase.GetCurrentMethod().Name);
+                response.ErrMessage = "210001";
+                return response;
             }
         }
     }
